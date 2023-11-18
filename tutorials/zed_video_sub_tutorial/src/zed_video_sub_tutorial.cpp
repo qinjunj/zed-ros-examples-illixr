be4cb18f25f840a2ecd1dee204913354158c2f86
Qinjun <qinjunj2@illinois.edu>
2023-11-17 23:38:49 -0600

    addded code to collect IMU and images

--- tutorials/zed_video_sub_tutorial/src/zed_video_sub_tutorial.cpp
+++ tutorials/zed_video_sub_tutorial/src/zed_video_sub_tutorial.cpp
@@ -24,71 +24,127 @@
  */
 
 #include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
 #include <sensor_msgs/Image.h>
+#include "opencv2/opencv.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include <fstream>
+#include <boost/filesystem.hpp>
+
+const boost::filesystem::path imu_path = "/home/qinjun/Research/dataset/imu0";
+std::ofstream imu_wt_file;
+const boost::filesystem::path cam0_path = "/home/qinjun/Research/dataset/cam0";
+const boost::filesystem::path cam1_path = "/home/qinjun/Research/dataset/cam1";
+const boost::filesystem::path cam0_data_path = "/home/qinjun/Research/dataset/cam0/data";
+const boost::filesystem::path cam1_data_path = "/home/qinjun/Research/dataset/cam1/data";
+std::ofstream cam0_wt_file;
+std::ofstream cam1_wt_file;
 
 /**
  * Subscriber callbacks. The argument of the callback is a constant pointer to the received message
  */
-
-void imageRightRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg)
+void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
-  ROS_INFO("Right Rectified image received from ZED - Size: %dx%d", msg->width, msg->height);
+    ROS_INFO("Accel: %.3f,%.3f,%.3f [m/s^2] - Ang. vel: %.3f,%.3f,%.3f [deg/sec] - Orient. Quat: %.3f,%.3f,%.3f,%.3f",
+            msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z, msg->angular_velocity.x,
+            msg->angular_velocity.y, msg->angular_velocity.z, msg->orientation.x, msg->orientation.y, msg->orientation.z,
+            msg->orientation.w);
+    
+    ros::Time timestamp = msg->header.stamp;
+    int64_t timestamp_ns = timestamp.toNSec();
+
+    imu_wt_file << timestamp_ns << "," << msg->angular_velocity.x << ","
+                                       << msg->angular_velocity.y << ","
+                                       << msg->angular_velocity.z << ","
+                                       << msg->linear_acceleration.x << ","
+                                       << msg->linear_acceleration.y << ","
+                                       << msg->linear_acceleration.z << std::endl;
 }
 
-void imageLeftRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg)
-{
-  ROS_INFO("Left Rectified image received from ZED - Size: %dx%d", msg->width, msg->height);
+void imageLeftRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg) {
+    ros::Time timestamp = msg->header.stamp;
+    int64_t timestamp_ns = timestamp.toNSec();
+
+    try
+    {
+        // Convert the ROS image message to an OpenCV image
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+
+        // Save the rectified grayscale image to a file
+        std::string filename = cam0_data_path.string() + "/" + std::to_string(timestamp_ns) + ".jpg";
+        cv::imwrite(filename, cv_ptr->image);
+
+        cam0_wt_file << timestamp_ns << "," << std::to_string(timestamp_ns) + ".jpg" << std::endl;
+
+        ROS_INFO("Rectified grayscale image saved at time %ld", timestamp_ns);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("CV_Bridge Exception: %s", e.what());
+    }
+
+}
+
+void imageRightRectifiedCallback(const sensor_msgs::Image::ConstPtr& msg) {
+    ros::Time timestamp = msg->header.stamp;
+    int64_t timestamp_ns = timestamp.toNSec();
+
+    try
+    {
+        // Convert the ROS image message to an OpenCV image
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+
+        // Save the rectified grayscale image to a file
+        std::string filename = cam1_data_path.string() + "/" + std::to_string(timestamp_ns) + ".jpg";
+        cv::imwrite(filename, cv_ptr->image);
+
+        cam1_wt_file << timestamp_ns << "," << std::to_string(timestamp_ns) + ".jpg" << std::endl;
+
+        ROS_INFO("Rectified grayscale image saved at time %ld", timestamp_ns);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("CV_Bridge Exception: %s", e.what());
+    }
+    
 }
 
 /**
  * Node main function
  */
-int main(int argc, char** argv)
-{
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "zed_video_subscriber");
+int main(int argc, char** argv) {
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+    // Node initialization
+    ros::init(argc, argv, "zed_video_subscriber");
+    ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called imageCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber subRightRectified =
-      n.subscribe("/zed/zed_node/right/image_rect_color", 10, imageRightRectifiedCallback);
-  ros::Subscriber subLeftRectified = n.subscribe("image", 10, imageLeftRectifiedCallback);
+    // Subscribers
+    ros::Subscriber subRightRectified = n.subscribe("/zedm/zed_node/right/image_rect_gray", 10,
+                                                    imageRightRectifiedCallback);
+    ros::Subscriber subLeftRectified  = n.subscribe("/zedm/zed_node/left/image_rect_gray", 10,
+                                                    imageLeftRectifiedCallback);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+    ros::Subscriber subImu = n.subscribe("/zedm/zed_node/imu/data", 100,
+                                                    imuCallback);
 
-  return 0;
+    boost::filesystem::create_directories(imu_path);
+    std::string imu_file = imu_path.string() + "/data.csv";
+    imu_wt_file.open(imu_file, std::ofstream::out);
+    imu_wt_file << "#timestamp [ns],w_x [rad s^-1],w_y [rad s^-1],w_z [rad s^-1],a_x [m s^-2],a_y [m s^-2],a_z [m s^-2]"
+                    << std::endl;
+    boost::filesystem::create_directories(cam0_path);
+    boost::filesystem::create_directories(cam1_path);
+    boost::filesystem::create_directories(cam0_data_path);
+    boost::filesystem::create_directories(cam1_data_path);
+    std::string cam0_file = cam0_path.string() + "/data.csv";
+    std::string cam1_file = cam1_path.string() + "/data.csv";
+    cam0_wt_file.open(cam0_file, std::ofstream::out);
+    cam1_wt_file.open(cam1_file, std::ofstream::out);
+    cam0_wt_file << "#timestamp [ns],filename" << std::endl;
+    cam1_wt_file << "#timestamp [ns],filename" << std::endl;
+
+    // Node execution
+    ros::spin();
+
+    return 0;
 }
